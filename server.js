f9e8e0be06eff04fd451f8072d48114ac78b6650
Junior <junior@Juniors-Mac-mini.local>
2021-01-19 23:04:41 -0600

    Added test to the endpoints

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.status(200).json({ api: "running" });
+});
+
+server.get('/api/projects', (req, res) => {
+    res.status(200).json({ works: true })
+});
+
+server.post('/api/projects', (req, res) => {
+    const project = req.body
+    res.status(201).json({ ...project, id: 1 })
+})
+
+server.delete('/api/projects/:id', (req, res) => {
+    const { id } = req.params
+    if (id > 1) {
+        res.status(404).json({ message: 'that is does not exist' })
+    }
+    else {
+        res.status(204).end()
+    }
+})
+
+server.get('api/projects/:id', (req, res) => {
+    const { id } = req.params
+    if (id > 1) {
+        res.status(400).json({ message: 'that is does not exist'})
+    } else {
+        res.status(200).json({
+            name: "unit4 - sprint3 -module4",
+            language: "javascript"
+        })
+    }
+})
+
+module.exports = server;
